648e57b79a7e2a2632fca3ae44821cdfb74af729
AshKiano <ash.kiano@gmail.com>
2023-06-25 15:11:39 +0200

    version 1.3

--- src/main/java/com/ashkiano/spigotweb/SpigotWeb.java
+++ src/main/java/com/ashkiano/spigotweb/SpigotWeb.java
@@ -7,15 +7,18 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-//TODO automaticky vytvářet www složku a v ní nějaký basic index.html
 public class SpigotWeb extends JavaPlugin {
     private Server server;
 
     @Override
     public void onEnable() {
+        // Save the default configuration if it does not exist
         this.saveDefaultConfig();
 
+        // Create a new metrics object with this plugin instance and the plugin ID
         Metrics metrics = new Metrics(this, 18807);
 
         // Get the port from the configuration file
@@ -24,12 +27,30 @@ public class SpigotWeb extends JavaPlugin {
         // Create a new Jetty server instance with the specified port
         server = new Server(port);
 
-        // Create a servlet context handler and set the resource base to the "www" directory
+        // Create a servlet context handler with session support
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
-        context.setResourceBase(new File("www").getAbsolutePath());
 
-        // Add a default servlet to handle requests
+        // Create the www directory if it doesn't exist
+        File wwwDir = new File("www");
+        if (!wwwDir.exists()) {
+            wwwDir.mkdir();
+        }
+
+        // Create the index.html file in the www directory with "Hello World" content if it doesn't exist
+        File indexFile = new File(wwwDir, "index.html");
+        if (!indexFile.exists()) {
+            try (FileWriter writer = new FileWriter(indexFile)) {
+                writer.write("<h1>Hello world</h1>");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Set the resource base to the absolute path of the www directory
+        context.setResourceBase(wwwDir.getAbsolutePath());
+
+        // Add a default servlet to handle requests and enable directory listing
         ServletHolder holder = context.addServlet(DefaultServlet.class, "/");
         holder.setInitParameter("dirAllowed", "true");
         server.setHandler(context);
