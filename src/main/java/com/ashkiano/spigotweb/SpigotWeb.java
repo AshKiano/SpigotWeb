153a7fe12320740bf2176766b69a2690862f31b6
AshKiano <ash.kiano@gmail.com>
2023-08-10 13:47:14 +0200

    version 1.11

--- src/main/java/com/ashkiano/spigotweb/SpigotWeb.java
+++ src/main/java/com/ashkiano/spigotweb/SpigotWeb.java
@@ -45,7 +45,7 @@ public class SpigotWeb extends JavaPlugin {
         // Call the server setup method
         setupServer();
 
-        System.out.println("Thank you for using the SpigotWeb plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://paypal.me/josefvyskocil");
+        this.getLogger().info("Thank you for using the SpigotWeb plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://paypal.me/josefvyskocil");
     }
 
     @Override
