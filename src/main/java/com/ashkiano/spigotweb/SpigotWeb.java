735f101c0d9f234d38b753b2b000d5136eb4e3ca
AshKiano <ash.kiano@gmail.com>
2023-08-23 20:45:18 +0200

    version 1.12

--- src/main/java/com/ashkiano/spigotweb/SpigotWeb.java
+++ src/main/java/com/ashkiano/spigotweb/SpigotWeb.java
@@ -11,12 +11,14 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 //TODO zmenit port a jazyk prikazem
@@ -46,6 +48,8 @@ public class SpigotWeb extends JavaPlugin {
         setupServer();
 
         this.getLogger().info("Thank you for using the SpigotWeb plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://paypal.me/josefvyskocil");
+
+        checkForUpdates();
     }
 
     @Override
@@ -162,6 +166,46 @@ public class SpigotWeb extends JavaPlugin {
         protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             System.out.println("Received request from " + req.getRemoteAddr());
             super.service(req, resp);
+        }
+    }
+
+    private void checkForUpdates() {
+        try {
+            String pluginName = this.getDescription().getName();
+            URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                if (jsonObject.has("error")) {
+                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                } else {
+                    String latestVersion = jsonObject.getString("latest_version");
+
+                    String currentVersion = this.getDescription().getVersion();
+                    if (currentVersion.equals(latestVersion)) {
+                        this.getLogger().info("This plugin is up to date!");
+                    } else {
+                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    }
+                }
+            } else {
+                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
         }
     }
 }
